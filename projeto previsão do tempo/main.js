4c98030aefc947982428a4ad19ca757580cdcdb9
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-08-14 11:09:04 -0300

    att

--- projeto previsão do tempo/main.js
+++ projeto previsão do tempo/main.js
@@ -1,16 +1,17 @@
 const timeElement = document.querySelector("#time");
 const dateElement = document.querySelector("#date");
-const infosElement = document.querySelector("#infos");
+const infosElement = document.querySelector("#infos-items");
 const timezone = document.querySelector("#time-zone");
-// const countryEl = document.querySelector('country');
-const weatherForecastEl = document.querySelector('weather-forecast');
+const weatherForecastEl = document.querySelector("#weather-forecast");
 const currentTempEl = document.querySelector("#current-temp");
 
-const days = ['Sunday', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
+const days = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Otu', 'Nov', 'Dec'];
+const time = new Date();
+
+getWeatherData()
 
 setInterval(() => {
-    const time = new Date();
     const month = time.getMonth();
     const date = time.getDate();
     const day = time.getDay();
@@ -34,7 +35,7 @@ setInterval(() => {
 
 }, 1000);
 
-const getWeatherData = () => {
+function getWeatherData() {
     navigator.geolocation.getCurrentPosition((success) => {
 
         // const latitude = success.coords.latitude;
@@ -46,67 +47,100 @@ const getWeatherData = () => {
 
         // console.log({latitude, longitude})
 
-        // const link = `https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=${apiKey}&include=minutely`
-
-        // const link = `https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=${apiKey}&units=M`
+        // const link = `https://api.weatherbit.io/v2.0/current?lat=${latitude}&lon=${longitude}&key=${apiKey}&include=minutely`;
+       
+        const linkForecast = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=M`
         
         // const link = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey2}`
-        
-        fetch(`${link}`)
+
+        // fetch(link)
+        // .then(res => res.json())
+        // .then(data => {
+
+        //     console.log(data)
+
+        //     showWeatherData(data);
+
+        // })
+
+        fetch(linkForecast)
         .then(res => res.json())
         .then(data => {
+
             console.log(data)
 
-            // console.log(data.data[0].clouds)
+            showWeatherForecast(data);
 
-            showWeatherData(data);
         })
 
     })
 }
 
-getWeatherData()
+// function showWeatherData(data) {
+//     const {rh, pop, clouds, sunrise, sunset, wind_spd, temp} = data.data[0];
+//     const {city_name} = data
+//     const {icon} = data.data[0].weather
+//     const today = time.getDay();
 
-function showWeatherData (data){
-    const {rh, pres, clouds, sunrise, sunset, wind_spd, temp, app_temp, city_name} = data.data[0];
-    const today = time.getDay();
+//     timezone.innerText = city_name;
 
-    timezone.innerHTML = city_name;
+//     infosElement.innerHTML = `
+//         <div class="infos-items">
+//             <p>Humidade</p>
+//             <p>${rh}%</p>
+//         </div>
+//         <div class="infos-items">
+//             <p>Chuva</p>
+//             <p>${pop}$</p>
+//         </div>
+//         <div class="infos-items">
+//             <p>Nuvens</p>
+//             <p>${clouds}%</p>
+//         </div>
+//         <div class="infos-items">
+//             <p>Veloc. do vento</p>
+//             <p>${wind_spd}</p>
+//         </div>
+//         <div class="infos-items">
+//             <p>Nascer do sol</p>
+//             <p>${sunrise}</p>
+//         </div>
+//         <div class="infos-items">
+//             <p>Por do sol</p>
+//             <p>${sunset}</p>
+//         </div>`;
 
-    currentTempEl.innerHTML = `
-        <div class="infos-items">
-            <p>Humidade</p>
-            <p>${rh}%</p>
-        </div>
-        <div class="infos-items">
-            <p>presurre</p>
-            <p>${pres}</p>
-        </div>
-        <div class="infos-items">
-            <p>Nuvens</p>
-            <p>${clouds}</p>
-        </div>
-        <div class="infos-items">
-            <p>Veloc. do vento</p>
-            <p>${wind_spd}</p>
-        </div>
-        <div class="infos-items">
-            <p>Nascer do sol</p>
-            <p>${window.moment(sunrise).format('HH:mm a')}</p>
-        </div>
-        <div class="infos-items">
-            <p>Por do sol</p>
-            <p>${window.moment(sunset).format('HH:mm a')}</p>
-        </div>`;
+//     currentTempEl.innerHTML = `
+//         <img src="https://www.weatherbit.io/static/img/icons/${icon}.png" alt="weather icon" class="w-icon">
+//         <div>
+//             <div class="day"><p>Hoje/${days[today]}</p></div>
+//             <div class="temp">Temp / ${temp}&#176;C</div>
+//             <div class="temp2">Max / ${app_max_temp}&#176;C</div>
+//             <div class="temp2">Min / ${app_min_temp}&#176;C</div>
+//         </div>`;
+// }
 
-    currentTempEl.innerHTML = `
-        <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
-        <div>
-            <div class="day"><p>Hoje/${days[today]}}</p></div>
-            <div class="temp">Temp - ${temp}&#176;C</div>
-            <div class="temp">Sens - ${app_temp}&#176;C</div>
-        </div>`;
+function showWeatherForecast(data) {
 
+    const forecastData = data.data;
 
-    weatherForecastEl.innerHTML = nextDayForcast;
+    if (forecastData.length !== 0) {
+
+        let forecastHTML = ""
+
+        forecastData.slice(1, 7).forEach((forecast) => {
+            const { app_max_temp, app_min_temp } = forecast;
+    
+            forecastHTML += `
+            <div class="weather-forecast-item">
+                <div class="day">Sáb</div>
+                <img src="https://www.weatherbit.io/static/img/icons/c01d.png" alt="weather icon" class="w-icon">
+                <div class="temp">Max / ${app_max_temp}&#176;C</div>
+                <div class="temp">Min / ${app_min_temp}&#176;C</div>
+            </div>
+            `;
+        });
+
+        weatherForecastEl.innerHTML = forecastHTML
+    } 
 }
